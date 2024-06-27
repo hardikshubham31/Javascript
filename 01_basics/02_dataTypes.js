4d58d1eeba0b7ce9b794f6c6bf1757ede1d71559
Hardik Shubham <141860138+hardikshubham31@users.noreply.github.com>
2024-06-27 15:52:12 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3); // code readability shhould be high
+
+let name = "hardik"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power of 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
