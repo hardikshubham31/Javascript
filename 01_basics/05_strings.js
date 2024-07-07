d60caa93274898370669f627c21b8183e49900cd
Hardik Shubham <141860138+hardikshubham31@users.noreply.github.com>
2024-07-07 16:40:28 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,3 +4,14 @@ const repoCount = 50
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hardikshubham')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('k'));
